7b0f57163699f64458d9275f39114d4565ead472
Steve Bauman <skbauman@sbcglobal.net>
2018-06-04 18:58:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.error("Halp!")
 console.log("This is where we start Logging.")
 console.warn("You may not want to do this action!")
-alert("why isnt' this thing letting me go the the next section?")
